fe6252d31fb852a38322979aa72d1f40bcedbe7f
dithof <aditramadhan0001@gmail.com>
2025-06-27 19:54:17 +0700

    single post

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,14 @@ Route::get('/blog', function () {
      $blog_posts = [
         [
             "title" => "Judul Post Pertama",
+            "slug" => "Judul-post-pertama",
             "author" => "Adithya ramadhan",
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
         ],
         [
             "title" => "Judul Post Kedua",
-            "author" => "nama saya adit",
+            "slug" => "Judul-post-kedua",
+            "author" => "dithof",
             "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod aspernatur culpa incidunt deleniti reiciendis laboriosam molestias ea libero officiis. Laborum consequuntur porro aliquid voluptatem a quas ab! Consectetur doloremque eaque fugit reprehenderit. Illo dignissimos fugiat, omnis, nulla voluptatum quo assumenda unde ab suscipit vero temporibus hic eius repudiandae veniam molestiae?",
         ]
     ];
@@ -34,5 +36,34 @@ Route::get('/blog', function () {
     return view('posts', [
         "title" => "Posts",
         "posts" => $blog_posts
+    ]);
+});
+// Halaman untuk single postAdd commentMore actions
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "Judul-post-pertama",
+            "author" => "Adithya ramadhan",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "Judul-post-kedua",
+            "author" => "dithof",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod aspernatur culpa incidunt deleniti reiciendis laboriosam molestias ea libero officiis. Laborum consequuntur porro aliquid voluptatem a quas ab! Consectetur doloremque eaque fugit reprehenderit. Illo dignissimos fugiat, omnis, nulla voluptatum quo assumenda unde ab suscipit vero temporibus hic eius repudiandae veniam molestiae?",
+        ]
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
     ]);
 });
