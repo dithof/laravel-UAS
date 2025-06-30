438870a01c577acf7f885f7bc2992a167b4750dd
dithof <aditramadhan0001@gmail.com>
2025-06-30 21:24:21 +0700

    route model blinding

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,22 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
-
 class PostController extends Controller
 {
     public function index()
     {
         return view('posts', [
-            "title" => "Posts",
-            "posts" => Post::all()
+            'title' => "Posts",
+            'posts' => Post::all()
         ]);
     }
-
-    public function show($slug)
+    public function show(Post $post)
     {
         return view('post', [
-            "title" => "Single Post",
-            "post" => Post::find($slug)
+            'title' => "Single Post",
+            'post' => $post
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Route::get('/about', function () {
 Route::get('/posts', [PostController::class, 'index']);
 
 // Halaman untuk single post
-Route::get('posts/{slug}', [PostController::class, 'show']);
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
