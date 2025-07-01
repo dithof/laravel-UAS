4f8f6cf6f8f9bfbc643f70b7de61ae7262ef0e2e
dithof <aditramadhan0001@gmail.com>
2025-07-01 20:23:48 +0700

    membuat halaman categories

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('container')
+    <h1 class="mb-5">All Categories</h1>
+    <ul>
+        @foreach ($categories as $category)
+            <li>
+                <h2><a href="/categories/{{ $category->slug }}">{{ $category->name }}</a></h2>
+            </li>
+        @endforeach
+    </ul>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,13 @@ Route::get('/posts', [PostController::class, 'index']);
 // Halaman untuk single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
+Route::get('/categories', function() {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
 Route::get('/categories/{category:slug}', function(Category $category) {
     return view('category', [
         'title' => $category->name,
