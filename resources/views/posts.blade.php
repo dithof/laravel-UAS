7f01f08346c70ed8c1bb6069161bf102f215ac76
dithof <aditramadhan0001@gmail.com>
2025-07-01 21:14:33 +0700

    fix code sebelumnya

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,14 @@
     <h2>
         <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
     </h2>
-    <p>By: <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+    <p>By: <a href="#" class="text-decoration-none">{{ $post->user->name }}</a>
+        in 
+        @if($post->category)
+            <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
+        @else
+            <span class="text-muted">No Category</span>
+        @endif
+    </p>
     <p>{{ $post->excerpt }}</p>
     <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
 </article>
