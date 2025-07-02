bc2ccf6c16082450c094724d706bfb591219dd69
dithof <aditramadhan0001@gmail.com>
2025-07-02 23:24:56 +0700

    input data user regis

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -15,11 +16,18 @@ class RegisterController extends Controller
     }
     public function store(Request $request)
     {
-        $validated = $request->validate([
+        $validatedData = $request->validate([
             'name' => 'required|max:30',
             'username' => 'required|min:3|max:30|unique:users',
             'email' => 'required|email:dns|unique:users',
             'password' => 'required|min:5|max:30'
+        ]);
+
+        User::create([
+            'name' => $validatedData['name'],
+            'username' => $validatedData['username'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password'])
         ]);
     }
 }
