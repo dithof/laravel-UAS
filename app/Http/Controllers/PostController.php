73de6c4b35940b06eda38949f5fffb97e449d26a
dithof <aditramadhan0001@gmail.com>
2025-07-02 20:03:53 +0700

    query scope

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,15 +8,11 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest();
-        if (request('search')) {
-            $posts->where('title', 'like', '%' . request('search') . '%')
-                  ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
+        
         return view('posts', [
             "title" => "All Posts",
             //'posts' => Post::all()
-            "posts" => $posts->get()
+            "posts" => Post::latest()->filter(request(['search']))->get()
         ]);
     }
     public function show(Post $post)
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,11 +26,24 @@ class Post extends Model
     // App\Models\Post::create([
     //     'title' => 'Judul Pertama',
     //     'category_id' => 1,
-    //     'author' => 'Arya Fadhila Azaya',
+    //     'author' => 'adithya ramadhan',
     //     'slug' => 'judul-pertama',
     //     'excerpt' => 'Lorem ipsum dolor sit amet pertama.',
     //     'body' => '<p>Lorem ipsum dolor sit amet pertama consectetur adipisicing elit. Harum, odit perferendis iure quisquam delectus ipsa autem esse libero suscipit molestiae molestias aut tempore iste qui ducimus laborum quidem itaque fugit reiciendis dicta dolores illo. Saepe sunt error repellat quam iusto ducimus recusandae ex unde! Quisquam quia nesciunt pariatur in iure ad dicta quaerat eligendi sed, aspernatur excepturi, dolor autem cupiditate.</p><p>Placeat dolore laboriosam voluptatum quisquam, minima, veniam odit, maiores blanditiis libero deleniti a amet commodi ipsum ab reprehenderit est impedit. Enim perferendis voluptatibus eum eos omnis dolore sed tenetur placeat illum praesentium distinctio deleniti voluptate dicta explicabo ab corrupti ullam quas magnam, blanditiis atque minima totam. Inventore quae quasi doloribus, sed itaque et veniam, officia ut eius repellat hic doloremque suscipit porro laboriosam odio nisi voluptas odit quam cum dignissimos. Enim, dolore totam? Rem aliquam non deserunt fugit, sapiente ratione iste deleniti quidem modi consequatur. Sit eligendi cupiditate voluptatibus molestias?</p>'
     // ]);
+
+    public function scopeFilter($query, array $filters)
+    {
+        // if (isset($filters['search']) ? $filters['search'] : false) {
+        //     return $query->where('title', 'like', '%' . $filters['search'] . '%')
+        //           ->orWhere('body', 'like', '%' . $filters['search'] . '%');
+        // }
+
+        $query->when($filters['search'] ?? false, function($query, $search) {
+            return $query->where('title', 'like', '%' . $search . '%')
+                         ->orWhere('body', 'like', '%' . $search . '%');
+        });
+    }
 
     public function category()
     {
