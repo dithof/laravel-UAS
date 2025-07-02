8795e221a05b82d84d3529f690ee06d2a29e1133
dithof <aditramadhan0001@gmail.com>
2025-07-02 15:40:56 +0700

    lazy eager loading menyelesaikan N+1 problem

--- routes/web.php
+++ routes/web.php
@@ -36,13 +36,13 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => 'Post By Category: ' . $category->name,
-        'posts' => $category->posts
+        'posts' => $category->posts->load('category', 'user')
     ]);
 });
 
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
         'title' => 'Posts By Author: ' . $author->name,
-        'posts' => $author->posts
+        'posts' => $author->posts->load('category', 'user')
     ]);
 });
