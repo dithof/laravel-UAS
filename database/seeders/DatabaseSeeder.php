31ea10aef361a70bd4fb3010d3793394c89128ec
dithof <aditramadhan0001@gmail.com>
2025-07-02 14:30:04 +0700

    Membuat factory untuk post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(5)->create();
 
         // User::create([
         //     'name' => 'Adithya ramadhan',
@@ -27,49 +26,53 @@ class DatabaseSeeder extends Seeder
 
         // User::create([
         //     'name' => 'Aditya',
-        //     'email' => 'adit123@gmail.com',
+        //     'email' => 'adit12@gmail.com',
         //     'password' => bcrypt('123')
         // ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'category_id' => 1,
-            'user_id' => 1,
-            'author' => 'Aditya',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet pertama.',
-            'body' => '<p>Lorem ipsum dolor sit amet pertama consectetur adipisicing elit. Harum, odit perferendis iure quisquam delectus ipsa autem esse libero suscipit molestiae molestias aut tempore iste qui ducimus laborum quidem itaque fugit reiciendis dicta dolores illo. Saepe sunt error repellat quam iusto ducimus recusandae ex unde! Quisquam quia nesciunt pariatur in iure ad dicta quaerat eligendi sed, aspernatur excepturi, dolor autem cupiditate.</p><p>Placeat dolore laboriosam voluptatum quisquam, minima, veniam odit, maiores blanditiis libero deleniti a amet commodi ipsum ab reprehenderit est impedit. Enim perferendis voluptatibus eum eos omnis dolore sed tenetur placeat illum praesentium distinctio deleniti voluptate dicta explicabo ab corrupti ullam quas magnam, blanditiis atque minima totam. Inventore quae quasi doloribus, sed itaque et veniam, officia ut eius repellat hic doloremque suscipit porro laboriosam odio nisi voluptas odit quam cum dignissimos. Enim, dolore totam? Rem aliquam non deserunt fugit, sapiente ratione iste deleniti quidem modi consequatur. Sit eligendi cupiditate voluptatibus molestias?</p>'
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'category_id' => 1,
+        //     'user_id' => 1,
+        //     'author' => 'Adithya Ramadhan',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet pertama.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet pertama consectetur adipisicing elit. Harum, odit perferendis iure quisquam delectus ipsa autem esse libero suscipit molestiae molestias aut tempore iste qui ducimus laborum quidem itaque fugit reiciendis dicta dolores illo. Saepe sunt error repellat quam iusto ducimus recusandae ex unde! Quisquam quia nesciunt pariatur in iure ad dicta quaerat eligendi sed, aspernatur excepturi, dolor autem cupiditate.</p><p>Placeat dolore laboriosam voluptatum quisquam, minima, veniam odit, maiores blanditiis libero deleniti a amet commodi ipsum ab reprehenderit est impedit. Enim perferendis voluptatibus eum eos omnis dolore sed tenetur placeat illum praesentium distinctio deleniti voluptate dicta explicabo ab corrupti ullam quas magnam, blanditiis atque minima totam. Inventore quae quasi doloribus, sed itaque et veniam, officia ut eius repellat hic doloremque suscipit porro laboriosam odio nisi voluptas odit quam cum dignissimos. Enim, dolore totam? Rem aliquam non deserunt fugit, sapiente ratione iste deleniti quidem modi consequatur. Sit eligendi cupiditate voluptatibus molestias?</p>'
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Kedua',
-            'category_id' => 1,
-            'user_id' => 1,
-            'author' => 'dithof',
-            'slug' => 'judul-kedua',
-            'excerpt' => 'Lorem ipsum dolor sit amet kedua.',
-            'body' => '<p>Lorem ipsum dolor sit amet kedua consectetur adipisicing elit. Harum, odit perferendis iure quisquam delectus ipsa autem esse libero suscipit molestiae molestias aut tempore iste qui ducimus laborum quidem itaque fugit reiciendis dicta dolores illo. Saepe sunt error repellat quam iusto ducimus recusandae ex unde! Quisquam quia nesciunt pariatur in iure ad dicta quaerat eligendi sed, aspernatur excepturi, dolor autem cupiditate.</p><p>Placeat dolore laboriosam voluptatum quisquam, minima, veniam odit, maiores blanditiis libero deleniti a amet commodi ipsum ab reprehenderit est impedit. Enim perferendis voluptatibus eum eos omnis dolore sed tenetur placeat illum praesentium distinctio deleniti voluptate dicta explicabo ab corrupti ullam quas magnam, blanditiis atque minima totam. Inventore quae quasi doloribus, sed itaque et veniam, officia ut eius repellat hic doloremque suscipit porro laboriosam odio nisi voluptas odit quam cum dignissimos. Enim, dolore totam? Rem aliquam non deserunt fugit, sapiente ratione iste deleniti quidem modi consequatur. Sit eligendi cupiditate voluptatibus molestias?</p>'
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'category_id' => 1,
+        //     'user_id' => 1,
+        //     'author' => 'dithof',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet kedua.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet kedua consectetur adipisicing elit. Harum, odit perferendis iure quisquam delectus ipsa autem esse libero suscipit molestiae molestias aut tempore iste qui ducimus laborum quidem itaque fugit reiciendis dicta dolores illo. Saepe sunt error repellat quam iusto ducimus recusandae ex unde! Quisquam quia nesciunt pariatur in iure ad dicta quaerat eligendi sed, aspernatur excepturi, dolor autem cupiditate.</p><p>Placeat dolore laboriosam voluptatum quisquam, minima, veniam odit, maiores blanditiis libero deleniti a amet commodi ipsum ab reprehenderit est impedit. Enim perferendis voluptatibus eum eos omnis dolore sed tenetur placeat illum praesentium distinctio deleniti voluptate dicta explicabo ab corrupti ullam quas magnam, blanditiis atque minima totam. Inventore quae quasi doloribus, sed itaque et veniam, officia ut eius repellat hic doloremque suscipit porro laboriosam odio nisi voluptas odit quam cum dignissimos. Enim, dolore totam? Rem aliquam non deserunt fugit, sapiente ratione iste deleniti quidem modi consequatur. Sit eligendi cupiditate voluptatibus molestias?</p>'
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'category_id' => 2,
-            'user_id' => 2,
-            'author' => 'hofdit',
-            'slug' => 'judul-ketiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet ketiga.',
-            'body' => '<p>Lorem ipsum dolor sit amet ketiga consectetur adipisicing elit. Harum, odit perferendis iure quisquam delectus ipsa autem esse libero suscipit molestiae molestias aut tempore iste qui ducimus laborum quidem itaque fugit reiciendis dicta dolores illo. Saepe sunt error repellat quam iusto ducimus recusandae ex unde! Quisquam quia nesciunt pariatur in iure ad dicta quaerat eligendi sed, aspernatur excepturi, dolor autem cupiditate.</p><p>Placeat dolore laboriosam voluptatum quisquam, minima, veniam odit, maiores blanditiis libero deleniti a amet commodi ipsum ab reprehenderit est impedit. Enim perferendis voluptatibus eum eos omnis dolore sed tenetur placeat illum praesentium distinctio deleniti voluptate dicta explicabo ab corrupti ullam quas magnam, blanditiis atque minima totam. Inventore quae quasi doloribus, sed itaque et veniam, officia ut eius repellat hic doloremque suscipit porro laboriosam odio nisi voluptas odit quam cum dignissimos. Enim, dolore totam? Rem aliquam non deserunt fugit, sapiente ratione iste deleniti quidem modi consequatur. Sit eligendi cupiditate voluptatibus molestias?</p>'
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'category_id' => 2,
+        //     'user_id' => 2,
+        //     'author' => 'aditya',
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet ketiga.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet ketiga consectetur adipisicing elit. Harum, odit perferendis iure quisquam delectus ipsa autem esse libero suscipit molestiae molestias aut tempore iste qui ducimus laborum quidem itaque fugit reiciendis dicta dolores illo. Saepe sunt error repellat quam iusto ducimus recusandae ex unde! Quisquam quia nesciunt pariatur in iure ad dicta quaerat eligendi sed, aspernatur excepturi, dolor autem cupiditate.</p><p>Placeat dolore laboriosam voluptatum quisquam, minima, veniam odit, maiores blanditiis libero deleniti a amet commodi ipsum ab reprehenderit est impedit. Enim perferendis voluptatibus eum eos omnis dolore sed tenetur placeat illum praesentium distinctio deleniti voluptate dicta explicabo ab corrupti ullam quas magnam, blanditiis atque minima totam. Inventore quae quasi doloribus, sed itaque et veniam, officia ut eius repellat hic doloremque suscipit porro laboriosam odio nisi voluptas odit quam cum dignissimos. Enim, dolore totam? Rem aliquam non deserunt fugit, sapiente ratione iste deleniti quidem modi consequatur. Sit eligendi cupiditate voluptatibus molestias?</p>'
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Keempat',
-            'category_id' => 2,
-            'user_id' => 2,
-            'author' => 'adit ramadhan',
-            'slug' => 'judul-keempat',
-            'excerpt' => 'Lorem ipsum dolor sit amet keempat.',
-            'body' => '<p>Lorem ipsum dolor sit amet keempat consectetur adipisicing elit. Harum, odit perferendis iure quisquam delectus ipsa autem esse libero suscipit molestiae molestias aut tempore iste qui ducimus laborum quidem itaque fugit reiciendis dicta dolores illo. Saepe sunt error repellat quam iusto ducimus recusandae ex unde! Quisquam quia nesciunt pariatur in iure ad dicta quaerat eligendi sed, aspernatur excepturi, dolor autem cupiditate.</p><p>Placeat dolore laboriosam voluptatum quisquam, minima, veniam odit, maiores blanditiis libero deleniti a amet commodi ipsum ab reprehenderit est impedit. Enim perferendis voluptatibus eum eos omnis dolore sed tenetur placeat illum praesentium distinctio deleniti voluptate dicta explicabo ab corrupti ullam quas magnam, blanditiis atque minima totam. Inventore quae quasi doloribus, sed itaque et veniam, officia ut eius repellat hic doloremque suscipit porro laboriosam odio nisi voluptas odit quam cum dignissimos. Enim, dolore totam? Rem aliquam non deserunt fugit, sapiente ratione iste deleniti quidem modi consequatur. Sit eligendi cupiditate voluptatibus molestias?</p>'
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Keempat',
+        //     'category_id' => 2,
+        //     'user_id' => 2,
+        //     'author' => 'fodith',
+        //     'slug' => 'judul-keempat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet keempat.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet keempat consectetur adipisicing elit. Harum, odit perferendis iure quisquam delectus ipsa autem esse libero suscipit molestiae molestias aut tempore iste qui ducimus laborum quidem itaque fugit reiciendis dicta dolores illo. Saepe sunt error repellat quam iusto ducimus recusandae ex unde! Quisquam quia nesciunt pariatur in iure ad dicta quaerat eligendi sed, aspernatur excepturi, dolor autem cupiditate.</p><p>Placeat dolore laboriosam voluptatum quisquam, minima, veniam odit, maiores blanditiis libero deleniti a amet commodi ipsum ab reprehenderit est impedit. Enim perferendis voluptatibus eum eos omnis dolore sed tenetur placeat illum praesentium distinctio deleniti voluptate dicta explicabo ab corrupti ullam quas magnam, blanditiis atque minima totam. Inventore quae quasi doloribus, sed itaque et veniam, officia ut eius repellat hic doloremque suscipit porro laboriosam odio nisi voluptas odit quam cum dignissimos. Enim, dolore totam? Rem aliquam non deserunt fugit, sapiente ratione iste deleniti quidem modi consequatur. Sit eligendi cupiditate voluptatibus molestias?</p>'
+        // ]);
+
+        User::factory(3)->create();
+
+        Post::factory(20)->create();
 
         Category::create([
             'name' => 'Web Programming',
