98cee4ee6f523eccfa91073651543f35f065f0da
dithof <aditramadhan0001@gmail.com>
2025-07-03 00:13:29 +0700

    penerapan middleware

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,12 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
 
 Route::get('/about', function () {
     return view('about', [
